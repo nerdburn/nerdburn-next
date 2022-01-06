eac6bf85c1771dc58ad868d0132974a90e659f41
nerdburn <shawn@nerdburn.com>
2022-01-06 14:58:18 -0800

    editing og

--- components/header.js
+++ components/header.js
@@ -6,7 +6,7 @@ export const Header = ({ posts }) =>
   <Head>
     <title>nerdburn</title>
     <meta property="og:title" content="Nerdburn"/>
-    <meta property="og:description" content="Shawn Adrian's blog"/>
+    <meta property="og:description" content="A collection of articles written by Shawn Adrian, a designer, developer, and blogger since 2000."/>
     <meta property="og:type" content="website"/>
     <meta property="og:url" content="https://nrdbrn.xyz"/>
     <meta property="og:image" content="https://nrdbrn.xyz/public/images/nerdburn-og-image.png"/>
