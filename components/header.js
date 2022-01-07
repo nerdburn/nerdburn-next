6703a5dce9853911c0698735919532e525f3c2f6
nerdburn <shawn@nerdburn.com>
2022-01-06 17:45:50 -0800

    adding og image

--- components/header.js
+++ components/header.js
@@ -9,7 +9,7 @@ export const Header = ({ posts }) =>
     <meta property="og:description" content="A collection of articles written by Shawn Adrian, a designer, developer, and blogger since 2000."/>
     <meta property="og:type" content="website"/>
     <meta property="og:url" content="https://nrdbrn.xyz"/>
-    <meta property="og:image" content="https://nrdbrn.xyz/public/images/nerdburn-og-image.png"/>
+    <meta property="og:image" content="https://storageapi.fleek.co/ae421f46-6f13-49bc-9855-221c09f6362a-bucket/nerdburn-og-image.png"/>
   </Head>
   <div className='header'>
     <h1><Link href='/'>n</Link></h1>
