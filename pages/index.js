d1ec76327229f2f5f710687a7b537a047ddcf32a
Shawn Adrian <shawn@nerdburn.com>
2025-07-03 11:53:41 -0700

    moving script out of head

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,8 @@ const Index = ({ post, posts }) => {
         <meta name="twitter:title" content="nerdburn" />
         <meta name="twitter:description" content="A collection of articles written by Shawn Adrian, a designer, developer, and blogger since 2000." />
         <meta name="twitter:image" content={`${process.env.NEXT_PUBLIC_DOMAIN}/images/nerdburn-og-image.png`} />
-        <Script src="https://analytics.ahrefs.com/analytics.js" data-key="dorXtQ0JnCiGIA6uB2uDHg" async></Script>
       </Head>
+      <Script src="https://analytics.ahrefs.com/analytics.js" data-key="dorXtQ0JnCiGIA6uB2uDHg" async></Script>
       <Post post={post} />
       <PostList posts={posts} />
     </>
